b7600fc21881a0ea2633992a7592a7fa6aa67cce
xingtingbiao <18551617816@163.com>
2019-06-03 13:54:40 +0800

    feature: gradle 管理的 Scala 和 Java工程

---
+++ src/test/scala/AppTest.scala
@@ -0,0 +1,19 @@
+import org.junit.Assert._
+import org.junit._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+    @Test
+    def testStr(): Unit = {
+        println("hello world")
+    }
+}
+
+
